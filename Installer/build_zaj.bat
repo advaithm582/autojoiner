5de16b38770f3a12d5b0b098a5ccd0efa22828e5
Advaith Menon <83835839+advaithm582@users.noreply.github.com>
2022-03-21 17:53:34 +0530

    Update build_zaj.bat

--- Installer/build_zaj.bat
+++ Installer/build_zaj.bat
@@ -2,7 +2,7 @@ cd ..
 CALL venv\Scripts\activate
 cd Installer
 py get_files.py vi
-pyinstaller ZoomAutojoinerGUI.py --onedir -i=ZoomAJIcon.ico --version-file=zaj_verinfo.txt --noconsole --noconfirm
+pyinstaller ZoomAutojoinerGUI.py --onedir --hidden-import tkinter --hidden-import tkinter.filedialog -i=ZoomAJIcon.ico --version-file=zaj_verinfo.txt --noconsole --noconfirm
 xcopy ZoomAJIcon.ico dist\ZoomAutojoinerGUI
 py get_files.py nsis
 pause
